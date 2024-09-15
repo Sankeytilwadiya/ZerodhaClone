a4a9a3875c2169ccc1169ade1d4c138f482d9e2d
Sankeytilwadiya <tilwadiyasankey86@gmail.com>
2024-09-15 18:47:47 +0530

    frontend add

--- src/landing_page/signup/Signup.js
+++ src/landing_page/signup/Signup.js
@@ -2,9 +2,13 @@ import React from 'react'
 function Signup() {
     return (
 
-        <h1>
-            Signup
-        </h1>
+       <form>
+        <lable name="username"/>
+<input  type="text" id ="username" />
+
+
+
+       </form>
       );
 }
 
